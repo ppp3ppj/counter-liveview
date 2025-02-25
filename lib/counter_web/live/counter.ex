abd2ffdaed3dca111e437e6e94bf3728aa5832f6
ppp3ppj <ppp.3ppj@gmail.com>
2025-02-25 20:21:25 +0700

    add: share state between clients

--- lib/counter_web/live/counter.ex
+++ lib/counter_web/live/counter.ex
@@ -1,16 +1,29 @@
 defmodule CounterWeb.Counter do
   use CounterWeb, :live_view
 
+  @topic "live"
+
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      CounterWeb.Endpoint.subscribe(@topic)
+    end
     {:ok, assign(socket, :val, 0)}
   end
 
   def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+    new_state = update(socket, :val, &(&1 + 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+    new_state = update(socket, :val, &(&1 - 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   def render(assigns) do
