e29242c652d05128157c5e15b2dc5ef08577f2d0
ppp3ppj <ppp.3ppj@gmail.com>
2025-03-03 19:18:30 +0700

    refactor: btn in html to use daisyui

--- lib/counter_web/live/counter_component.ex
+++ lib/counter_web/live/counter_component.ex
@@ -2,8 +2,8 @@ defmodule CounterComponent do
   use Phoenix.LiveComponent
 
   # Avoid duplicating tailwind classes and show hot to inline a function call:
-  def btn(color) do
-    "text-6xl pb-2 w-20 rounded-lg bg-#{color}-500 hover:bg-#{color}-600"
+  def btn(type) do
+    "btn btn-active  btn-#{type} text-2xl btn-lg"
   end
 
   def render(assigns) do
@@ -11,8 +11,8 @@ defmodule CounterComponent do
     <div class="text-center">
       <h1 class="text-center text-4xl font-bold">Counter: <%= @val %> </h1>
       <p class="text-center">
-        <button class={btn("red")} phx-click="dec">-</button>
-        <button class={btn("green")} phx-click="inc">+</button>
+        <button class={btn("primary")} phx-click="dec">-</button>
+        <button class={btn("error")} phx-click="inc">+</button>
       </p>
     </div>
     """
